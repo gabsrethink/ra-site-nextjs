783724d42d03cdf7b2d629e266b248896ad1218c
gabsrethink <gabriel.melo@rethink.dev>
2022-12-09 16:09:17 -0300

    axios url fix

--- utils/requester.ts
+++ utils/requester.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const instance = axios.create({
-  // baseURL: "https://strapi-production-67d1.up.railway.app/api/",
-  baseURL: "http://localhost:1337/api/",
+  baseURL: "https://strapi-production-67d1.up.railway.app/api/",
+  //baseURL: "http://localhost:1337/api/",
   timeout: 15000,
 });
