2c34e5b75eee340e0840e7b55a670e1af66ed0f6
gabsrethink <gabriel.melo@rethink.dev>
2023-01-03 16:04:01 -0300

    header fix

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 import styles from "./header.module.css";
 
@@ -11,13 +12,16 @@ export default function Nav({ props: data }: any) {
               <img src={val.logo.url} alt={val.logo.name} />
             </div>
             <div className={styles.header_buttons}>
-              {val.links.map((link: any, i: any) => {
+              {/* {val.links.map((link: any, i: any) => {
                 return (
                   <a key={i} href={`#${link.name}`}>
                     {link.name}
                   </a>
                 );
-              })}
+              })} */}
+              <Link href={"/"}>Home</Link>
+              <a href={"/#Sobre"}>Sobre</a>
+              <Link href={"/projects"}>Projetos</Link>
             </div>
           </div>
         );
