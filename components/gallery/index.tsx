707bd92a5e3af47fe8d0b388e3db229301d40108
gabsrethink <gabriel.melo@rethink.dev>
2023-01-06 14:57:08 -0300

    gallery map key fix

--- components/gallery/index.tsx
+++ components/gallery/index.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import styles from "./gallery.module.css";
-import Button from "../button";
 
 export default function Gallery({ props: data }: any) {
   return (
     <>
       {data.map((val: any, i: any) => {
         return (
-          <div className={styles.carousel_container}>
+          <div className={styles.carousel_container} key={i}>
             <Carousel variant="dark" fade>
               {val.promGallery.map((post: any, i: any) => {
                 return (
