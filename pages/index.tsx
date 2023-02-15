3a1c3a2bf9c9635029797a53e1e42cb134423c80
gabsrethink <gabriel.melo@rethink.dev>
2023-02-15 12:27:03 -0300

    GA second test

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,21 @@ import TitleContent from "../components/titleContent";
 import TwoColumnsSection from "../components/twoColumnsSection";
 import { instance } from "../utils/requester";
 import { NextPageWithLayout } from "./_app";
+import Script from "next/script";
 
 const Home: NextPageWithLayout = ({ data }: any) => {
   return (
     <div>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-THJNXZ0HED"
+      ></Script>
+      <Script>
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-THJNXZ0HED')`}
+      </Script>
       <TitleContent props={data} />
       <TwoColumnsSection props={data} />
       <ScrollArrow />
